cf0b150bd29e576aba6b3304c9ecacbba79d9d48
IAmDevCT <timothy.ofili@charisol.io>
2022-12-08 07:44:14 +0100

    base styles and tailwind initial setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/kumbh-sans';
+import '@fontsource/kumbh-sans/400.css';
+import '@fontsource/kumbh-sans/700.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
