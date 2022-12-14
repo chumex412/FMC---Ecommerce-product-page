4f016f0d356af5cb0d2cf80a266e49f1fc4b6f2d
IAmDevCT <timothy.ofili@charisol.io>
2022-12-14 18:37:53 +0100

    Adds more icon assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,30 +1,32 @@
 import { ReactComponent as BrandLogo } from './svgs/logo.svg';
 import { ReactComponent as CartIcon } from './svgs/icon-cart.svg';
+import { ReactComponent as WhiteCartIcon } from './svgs/icon-cart copy.svg';
+import { ReactComponent as PlusIcon } from './svgs/icon-plus.svg';
+import { ReactComponent as MenuIcon } from './svgs/icon-menu.svg';
+import { ReactComponent as MinusIcon } from './svgs/icon-minus.svg';
+import { ReactComponent as DeleteIcon } from './svgs/icon-delete.svg';
+import { ReactComponent as CloseIcon } from './svgs/icon-close.svg';
 
 import AvatarImg from './others/image-avatar.png';
-import product1Big from './others/image-product-1.jpg'
-import product1Small from './others/image-product-1-thumbnail.jpg'
-import product2Big from './others/image-product-1.jpg'
-import product2Small from './others/image-product-2-thumbnail.jpg'
-import product3Big from './others/image-product-1.jpg'
-import product3Small from './others/image-product-3-thumbnail.jpg'
-import product4Big from './others/image-product-4.jpg'
-import product4Small from './others/image-product-4-thumbnail.jpg'
+import product1Big from './others/image-product-1.jpg';
+import product1Small from './others/image-product-1-thumbnail.jpg';
+import product2Big from './others/image-product-1.jpg';
+import product2Small from './others/image-product-2-thumbnail.jpg';
+import product3Big from './others/image-product-1.jpg';
+import product3Small from './others/image-product-3-thumbnail.jpg';
+import product4Big from './others/image-product-4.jpg';
+import product4Small from './others/image-product-4-thumbnail.jpg';
 
 export const imgs = {
-  AvatarImg,
-  product1Big,
-  product2Big,
-  product3Big,
-  product4Big,
-  product1Small,
-  product2Small,
-  product3Small,
-  product4Small
-} 
+	AvatarImg,
+	product1Big,
+	product2Big,
+	product3Big,
+	product4Big,
+	product1Small,
+	product2Small,
+	product3Small,
+	product4Small
+};
 
-
-export {
-  BrandLogo,
-  CartIcon
-}
+export { BrandLogo, CartIcon, CloseIcon, DeleteIcon, MenuIcon, MinusIcon, PlusIcon, WhiteCartIcon };
